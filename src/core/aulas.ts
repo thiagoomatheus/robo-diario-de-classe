c00d81a8175ec84d7c1586a93849aa16f1c71027
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-05-27 00:19:18 -0300

    adicionar headers

--- src/core/aulas.ts
+++ src/core/aulas.ts
@@ -622,6 +622,7 @@ export async function registrarAulaViaRequest(config: ConfigAula) {
                                 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36',
                             },
                             body: formData.toString(),
+                            credentials: 'include'
                         });
 
                         const contentType = response.headers.get('content-type');
